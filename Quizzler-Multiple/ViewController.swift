ae6ba2847594466a8c6f490403ae8cd945584209
canknbr <cankanbur1@gmail.com>
2023-04-07 16:22:37 +0300

    added sample

--- Quizzler-Multiple/ViewController.swift
+++ Quizzler-Multiple/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var quizBrain = QuizBrain()
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var choiceThreeButton: UIButton!
+    @IBOutlet weak var choiceTwoButton: UIButton!
+    @IBOutlet weak var choiceOneButton: UIButton!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        update()
     }
-
-
+    
+    
+    @IBAction func selectButton(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        
+        let userRight = quizBrain.checkAnswer(userAnswer)
+        
+        if userRight {
+            sender.backgroundColor = .green
+        }else{
+            sender.backgroundColor = .red
+        }
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(update), userInfo: nil, repeats: false)
+    }
+    
+    
+    @objc func update(){
+        questionLabel.text = quizBrain.getQuestion()
+        let answers = quizBrain.getAnswer()
+        choiceOneButton.setTitle(answers[0], for: .normal)
+        choiceTwoButton.setTitle(answers[1], for: .normal)
+        choiceThreeButton.setTitle(answers[2], for: .normal)
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        choiceOneButton.backgroundColor = .clear
+        choiceTwoButton.backgroundColor = .clear
+        choiceThreeButton.backgroundColor = .clear
+    }
+    
 }
 
